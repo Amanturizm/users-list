23fd8a18164c581a2cc6ecdce59e6237d8ad5b9b
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2024-03-01 20:06:11 +0600

    Added application toolbar and ability to reload user list

--- src/script.js
+++ src/script.js
@@ -1,25 +1,42 @@
 const API_URL = 'https://jsonplaceholder.typicode.com/';
 
+const reloadBtn = document.getElementsByClassName('reload')[0];
+
+const togglePreloader = (isNone) => {
+  setTimeout(() => {
+    reloadBtn.style.animation = isNone ? 'none' : 'reload 600ms infinite ease-in-out'
+    reloadBtn.style.cursor = isNone ? 'pointer' : 'not-allowed';
+    reloadBtn.disabled = !isNone;
+  }, isNone ? 500 : 0);
+};
+
 const fetchData = async (url, init) => {
-  const response = await fetch(url, init);
+  togglePreloader();
+  const response = await fetch(url, init)
+    .catch(() => togglePreloader(true))
+    .finally(() => togglePreloader(true));
+
   return await response.json();
 };
 
 const cardsUser = document.getElementById('cards-user');
 
-const createUserCard = ({ username, email, phone }) => {
-  return `
+const setUsers = async () => {
+  const data = await fetchData(API_URL + 'users');
+
+  cardsUser.innerHTML = data.map(({username, email, phone}) => {
+    return `
     <div class="card-user">
       <h3>${username}</h3>
       <p>${email}</p>
       <p>${phone}</p>
     </div>
   `;
+  }).join('');
 };
 
-(async () => {
-  const data = await fetchData(API_URL + 'users');
+void setUsers();
 
-  const userCardsHTML = data.map(createUserCard).join('');
-  cardsUser.innerHTML = userCardsHTML;
-})();
+reloadBtn.addEventListener('click', () => {
+  void setUsers();
+});
